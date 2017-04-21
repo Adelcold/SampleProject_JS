30a080cac88bc16117c79d6c1e2587583df4da09
Adel Hassanvand <Adel Hassanvand>
2017-04-21 16:51:15 +1000

    loadajax

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 
-data = '[{"operation":"Please select..."}, {"operation":"Exp2"}, {"operation":"Sum"}, {"operation":"Minus"}, {"operation":"Multiple"}]';
+// data = '[{"operation":"Please select..."}, {"operation":"Exp2"}, {"operation":"Sum"}, {"operation":"Minus"}, {"operation":"Multiple"}]';
 
-function addOptions() {
-    var jsonArray = JSON.parse(data);
-    var select = document.getElementById('dd');
-    var option;
 
-    for (var i = 0; i < jsonArray.length; i++) {
-        option = document.createElement('option');
-        option.text = jsonArray[i]["operation"];
-        select.add(option);
-    }
+function loadJSON() {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'op2.json', true);
+    xobj.send();
+
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
 }
+
+function init() {
+    loadJSON(function (response) {
+        // Parse JSON string into object
+        var actual_JSON = JSON.parse(response);
+        document.getElementById("test").innerHTML = actual_JSON;
+    });
+
+    
+}
+init();
+console.log(data);
+
+
+
+//  function addOptions() {
+//         var jsonArray = JSON.parse(xobj.responseText);
+//         var select = document.getElementById('dd');
+//         var option;
+
+//         for (var i = 0; i < jsonArray.length; i++) {
+//             option = document.createElement('option');
+//             option.text = jsonArray[i]["operation"];
+//             select.add(option);
+//         }
+//     }
 
 
         //options += '<option value ="' + data[i] + '">' + '</option>';
